b600882e2e57606d2208324e4d4e079d1938d9ea
Serhii Babii <babiysergey1988@gmail.com>
2019-08-23 17:30:34 +0300

    Update todoData

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,9 +6,9 @@ import TodoList from './TodoList';
 import SearchPanel from './SearchPanel';
 
 const todoData = [
-  { id: 1, text: 'Wake Up' },
-  { id: 2, text: 'Drink coffee' },
-  { id: 3, text: 'Go to work' },
+  { id: 1, text: 'Wake Up', important: true, done: false },
+  { id: 2, text: 'Drink coffee', important: false, done: false },
+  { id: 3, text: 'Go to work', important: false, done: false },
 ];
 
 const App = () => {
