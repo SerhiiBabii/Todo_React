99d7121ed173fc015ec9d16bf6f32e9bf8786a63
Serhii Babii <babiysergey1988@gmail.com>
2019-08-24 14:26:13 +0300

    Finished files AddForm.js and addForm.css

--- src/Components/AddForm/AddForm.js
+++ src/Components/AddForm/AddForm.js
@@ -2,16 +2,38 @@ import React, { Component } from 'react';
 import './addForm.css';
 
 export default class AddForm extends Component {
+  state = {
+    text: '',
+  };
+
+  onTextChange = e => {
+    this.setState({
+      text: e.target.value,
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const { text } = this.state;
+    this.setState({ text: '' });
+    const ch = this.props.addItem || (() => {});
+    ch(text);
+  };
+
   render() {
     return (
-      <div className="add-form">
-        <button
-          className="btn btn-outline-secondary"
-          onClick={() => this.props.addItem('Drink green tea')}
-        >
+      <form className="bottom-panel d-flex" onSubmit={this.onSubmit}>
+        <input
+          className="form-control new-todo-text"
+          type="text"
+          value={this.state.text}
+          onChange={this.onTextChange}
+          placeholder="Write your task"
+        />
+        <button type="submit" className="btn btn-outline-secondary">
           Add item
         </button>
-      </div>
+      </form>
     );
   }
 }
