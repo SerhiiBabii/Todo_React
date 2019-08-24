d7575f102f594d7d0bc47fc9cf7f95ce7d39effd
Serhii Babii <babiysergey1988@gmail.com>
2019-08-24 10:06:01 +0300

    Added change TodoList

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -2,14 +2,24 @@ import React from 'react';
 import TodoListItem from '../TodoListItem';
 import './todoList.css';
 
-const TodoList = ({ todoData, deleteItem }) => {
+const TodoList = ({
+  todoData,
+  deleteItem,
+  onToggleDone,
+  onToggleImportant,
+}) => {
   return (
     <ul className="list-group todo-list">
       {todoData.map(item => {
         const { id, ...itemsProps } = item;
         return (
           <li key={id} className="list-group-item">
-            <TodoListItem {...itemsProps} deleteItem={() => deleteItem(id)} />
+            <TodoListItem
+              {...itemsProps}
+              deleteItem={() => deleteItem(id)}
+              onToggleImportant={() => onToggleImportant(id)}
+              onToggleDone={() => onToggleDone(id)}
+            />
           </li>
         );
       })}
