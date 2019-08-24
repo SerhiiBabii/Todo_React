e0871a6eab9f8bf7c37a01271ca26fb887d424a6
Serhii Babii <babiysergey1988@gmail.com>
2019-08-24 14:29:46 +0300

    Finished file ItemStatusFilter.js

--- src/Components/ItemStatusFilter/ItemStatusFilter.js
+++ src/Components/ItemStatusFilter/ItemStatusFilter.js
@@ -1,19 +1,25 @@
 import React from 'react';
 
-const ItemStatusFilter = () => {
-  return (
-    <div className="btn-group">
-      <button type="button" className="btn btn-info">
-        All
+const filterButton = [
+  { name: 'all', text: 'All' },
+  { name: 'active', text: 'Active' },
+  { name: 'done', text: 'Done' },
+];
+
+const ItemStatusFilter = ({ filter, onFilter = () => {} }) => {
+  const buttons = filterButton.map(({ name, text }) => {
+    const isActive = name === filter;
+    const className =
+      'btn ' + (isActive ? 'btn-info' : 'btn-outline-secondary');
+
+    return (
+      <button key={name} onClick={() => onFilter(name)} className={className}>
+        {text}
       </button>
-      <button type="button" className="btn btn-outline-secondary">
-        Active
-      </button>
-      <button type="button" className="btn btn-outline-secondary">
-        Done
-      </button>
-    </div>
-  );
+    );
+  });
+
+  return <div className="btn-group">{buttons}</div>;
 };
 
 export default ItemStatusFilter;
