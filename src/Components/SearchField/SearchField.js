b17a0d54a263d48d3c05276abcc4c0984aebe7a0
Serhii Babii <babiysergey1988@gmail.com>
2019-08-24 14:30:39 +0300

    Finished files SearchField.js and searchField.css

--- src/Components/SearchField/SearchField.js
+++ src/Components/SearchField/SearchField.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './searchField.css';
 
-const SearchField = () => {
-  return (
-    <input
-      type="text"
-      className="form-control search-input"
-      placeholder="Write to search"
-    />
-  );
-};
+export default class SearchField extends Component {
+  state = {
+    searchText: '',
+  };
 
-export default SearchField;
+  onSearcTextChange = e => {
+    const { onSearch = () => {} } = this.props;
+    this.setState({
+      searchText: e.target.value,
+    });
+
+    onSearch(e.target.value);
+  };
+
+  render() {
+    return (
+      <input
+        type="text"
+        className="form-control search-input"
+        placeholder="Write text for search"
+        value={this.state.searchText}
+        onChange={this.onSearcTextChange}
+      />
+    );
+  }
+}
