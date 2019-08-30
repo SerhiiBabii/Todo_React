a0df3bc5e03449b13f663d3c43ca4ed2559feef0
Serhii Babii <babiysergey1988@gmail.com>
2019-08-30 08:48:43 +0300

    Added error field in AddForm

--- src/Components/AddForm/AddForm.js
+++ src/Components/AddForm/AddForm.js
@@ -4,36 +4,48 @@ import './addForm.css';
 export default class AddForm extends Component {
   state = {
     text: '',
+    error: '',
   };
 
   onTextChange = e => {
     this.setState({
       text: e.target.value,
+      error: '',
     });
   };
 
   onSubmit = e => {
     e.preventDefault();
     const { text } = this.state;
-    this.setState({ text: '' });
-    const ch = this.props.addItem || (() => {});
-    ch(text);
+    if (text) {
+      this.setState({ text: '' });
+      const ch = this.props.addItem || (() => {});
+      ch(text);
+    } else {
+      this.setState({
+        error: 'Сheck data entered in the field',
+      });
+    }
   };
 
   render() {
+    const { text, error } = this.state;
     return (
-      <form className="bottom-panel d-flex" onSubmit={this.onSubmit}>
-        <input
-          className="form-control new-todo-text"
-          type="text"
-          value={this.state.text}
-          onChange={this.onTextChange}
-          placeholder="Write your task"
-        />
-        <button type="submit" className="btn btn-outline-secondary">
-          Add item
-        </button>
-      </form>
+      <div>
+        <form className="bottom-panel d-flex" onSubmit={this.onSubmit}>
+          <input
+            className="form-control new-todo-text"
+            type="text"
+            value={text}
+            onChange={this.onTextChange}
+            placeholder="Write your task"
+          />
+          <button type="submit" className="btn btn-outline-secondary">
+            Add item
+          </button>
+        </form>
+        {error ? <span className="bottom-panel__error">{error}</span> : null}
+      </div>
     );
   }
 }
